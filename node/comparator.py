60f5f0671ce07a32acc62493af87204bb5d9750e
Eugene Minov <minov.eug@gmail.com>
2019-06-10 18:27:00 +0300

    Migration package refactoring

--- node/comparator.py
+++ node/comparator.py
@@ -60,7 +60,8 @@ class NodeComparator(NodeExecutor):
                 self.retrieve_tl_from_old_node(commands_fifo_path, json_node, eq)
                 self.retrieve_h_tl_from_old_node(commands_fifo_path, json_node, json_ignored_node, eq)
                 self.retrieve_h_p_from_old_node(commands_fifo_path, json_node, json_ignored_node, eq)
-        except:
+        except Exception as e:
+            print(e)
             self.clear(node_handle)
             assert False, "Reassert "
 
